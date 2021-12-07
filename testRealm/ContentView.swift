6ad0f3589f8b9cec32408bb71d20b9631996a5c7
k2dliaxri <k2dliaxri@hotmail.com>
2021-12-07 11:19:56 +0800

    Initial Commit

---
+++ testRealm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testRealm
+//
+//  Created by hying on 2021/12/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testRealm/testRealmApp.swift
@@ -0,0 +1,17 @@
+//
+//  testRealmApp.swift
+//  testRealm
+//
+//  Created by hying on 2021/12/7.
+//
+
+import SwiftUI
+
+@main
+struct testRealmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
